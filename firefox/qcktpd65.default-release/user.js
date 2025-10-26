445a0184dc5db75ce8b400fc71d8713791fca489
Sabi0407 <github.pointer228@passmail.net>
2025-10-26 07:38:10 +0100

    fichier et dossier supprimer

--- firefox/qcktpd65.default-release/user.js
+++
@@ -1,6 +0,0 @@
-// Custom preferences managed by Codex
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.fullscreen.autohide", false);
-user_pref("browser.toolbars.bookmarks.visibility", "always");
-user_pref("browser.fullscreen.animateUp", 0);
-user_pref("browser.fullscreen.toolbar.visible", true);
