f3a68f41ae185f23e862798fcdab9f52ee0ffca2
Sabi0407 <github.pointer228@passmail.net>
2025-10-27 23:47:53 +0100

    update

--- .config/Scripts/toggle-layout.sh
+++ .config/Scripts/toggle-layout.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Script pour basculer entre les layouts Dwindle et Master
 
-CURRENT_LAYOUT=$(hyprctl getoption general:layout | grep "^layout:" | awk '{print $2}')
+CURRENT_LAYOUT=$(hyprctl getoption general:layout | grep "^str:" | awk '{print $2}')
 
 if [ "$CURRENT_LAYOUT" = "dwindle" ]; then
     # Passer à Master
     hyprctl keyword general:layout master
-    notify-send "Layout" "Master (Fenêtre principale)" -i view-restore -t 3000
+    notify-send "Layout" "Master (Fenêtre principale)" -i view-restore -t 2000
 else
     # Passer à Dwindle
     hyprctl keyword general:layout dwindle
-    notify-send "Layout" "Dwindle (Tiling flexible)" -i view-grid -t 3000
+    notify-send "Layout" "Dwindle (Tiling flexible)" -i view-grid -t 2000
 fi
